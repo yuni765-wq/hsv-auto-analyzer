a3649873b5cb30efa1f2d108ee9e8983169d550a
yuni765-wq <yuni765@gmail.com>
2025-10-15 02:43:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -268,9 +268,9 @@ centered = smoothed - stable_mean
 amp = np.abs(centered)
 min_frames = max(1, int((10.0 / 1000.0) * fps))
 
-    above = amp > th_on
-    below = amp < th_off
-    onset_index = offset_index = None
+above = amp > th_on
+below = amp < th_off
+onset_index = offset_index = None
 
     cnt = 0
     for i in range(len(above)):
@@ -386,6 +386,7 @@ if uploaded:
         st.pyplot(fig)
 else:
     st.info("분석할 파일을 업로드하면 자동으로 계산됩니다.")
+
 
 
 
